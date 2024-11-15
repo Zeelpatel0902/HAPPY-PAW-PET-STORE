302b6f5c723124b20bb50e051ba7bd76b3839139
Zeelpatel0902 <163676013+Zeelpatel0902@users.noreply.github.com>
2024-11-15 12:31:17 -0500

    happypawpetstore_client

--- apps.py
+++ apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class happypawpetstoreAdminConfig(AppConfig):
+class happypawpetstoreClientConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'happypawpetstore_Admin'
+    name = 'happypawpetstore_Client'
